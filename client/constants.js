07071426b13c0c4a96ab77990acda65da700bc6a
Anukritik007 <anukritik77@gmail.com>
2021-08-14 21:15:10 +0530

    Deploy Webpack app to Heroku

--- client/constants.js
+++ client/constants.js
@@ -1,3 +1,12 @@
-export const CATEGORIES_URL = "http://localhost:5000/categories";
-export const PRODUCTS_URL = "http://localhost:5000/products";
-export const BANNERS_URL = "http://localhost:5000/banners";
+let url;
+if (process.env.NODE_ENV === "development") {
+  url = "http://localhost:5000";
+}
+
+if (process.env.NODE_ENV === "production") {
+  url = "https://sabka-bazaar-demoapp.herokuapp.com";
+}
+
+export const CATEGORIES_URL = `${url}/categories`;
+export const PRODUCTS_URL = `${url}/products`;
+export const BANNERS_URL = `${url}/banners`;
